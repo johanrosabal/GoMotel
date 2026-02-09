df1bde66322d17fce9d0e47722dc16381ea647c5
Johan Manuel Rosabal <johan.manuel.rosabal@gmail.com>
2026-02-09 22:12:07 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-860739249-376ef",
+  "appId": "1:569894077193:web:10fa3fb5c0777b335f09d8",
+  "apiKey": "AIzaSyC0ipsOqNI5GWIxr-7CiIRtjSzarMZT-zY",
+  "authDomain": "studio-860739249-376ef.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "569894077193"
+};
