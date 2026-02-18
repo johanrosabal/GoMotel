4149198bf6b4f765550088a31571ef0d88896c7b
Johan Manuel Rosabal <johan.manuel.rosabal@gmail.com>
2026-02-18 00:47:57 +0000

    Quitar Icono (_for element <AppLogo>_)

--- src/components/TopNav.tsx
+++ src/components/TopNav.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import Link from 'next/link';
-import AppLogo from './AppLogo';
 import UserMenu from './UserMenu';
 import { useFirebase } from '@/firebase';
 import { ThemeToggle } from './ThemeToggle';
@@ -12,8 +11,7 @@ export default function TopNav() {
   return (
     <header className="sticky top-0 z-30 w-full border-b bg-background">
         <div className="container flex h-16 items-center">
-            <Link href={user ? '/dashboard' : '/'} className="mr-auto flex items-center space-x-2">
-              <AppLogo className="h-6 w-6 text-primary" />
+            <Link href={user ? '/dashboard' : '/'} className="mr-auto flex items-center">
               <span className="font-bold text-lg">Go Motel</span>
             </Link>
             <div className="flex items-center space-x-4">
